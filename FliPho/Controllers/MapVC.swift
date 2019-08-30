ce19f2710a6cc0b4977a91f40b273bb385e78066
Petre Vane <petre.vane@gmail.com>
2019-08-30 12:43:38 +0200

    Added project struture and necessary controller-files

---
+++ FliPho/Controllers/MapVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapVC.swift
+//  FliPho
+//
+//  Created by Petre Vane on 30/08/2019.
+//  Copyright © 2019 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
