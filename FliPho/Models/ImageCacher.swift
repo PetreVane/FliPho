3839768c0c0d6fff6025d8eb556475a26abcf580
Petre Vane <petre.vane@gmail.com>
2019-09-27 16:27:33 +0200

    xcode restart after crash: Cache file cannod be found anymore; added new caching model file

---
+++ FliPho/Models/ImageCacher.swift
@@ -0,0 +1,25 @@
+//
+//  ImageCacher.swift
+//  FliPho
+//
+//  Created by Petre Vane on 27/09/2019.
+//  Copyright © 2019 Petre Vane. All rights reserved.
+//
+
+import Foundation
+
+
+class ImageCacher: Operation {
+    
+    let photoRecord: PhotoRecord
+    
+    init(photoRecord: PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    
+    
+    override func main () {
+        
+        
+    }
+}
